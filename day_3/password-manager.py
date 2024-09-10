e376d15f15fb492478351450d7e96386e57a494c
Helen <hcardoso103@gmail.com>
2024-09-09 21:43:09 -0400

    new file: day_3/password-manager.py note: I am working around with the hashlib and getpass library :D

---
+++ day_3/password-manager.py
@@ -0,0 +1,39 @@
+import hashlib
+import getpass
+
+# Empty dictionary, this is used to store the key value pairs of the username and password
+password_manager = {}
+
+def create_account():
+    username = input('Enter your desired username: ')
+    password = getpass.getpass('Enter your desired password: ')
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    password_manager[username] = hashed_password
+    print('Account created successfully!')
+
+
+def login():
+    username = input('Enter your username: ')
+    password = getpass.getpass('Enter your password: ')
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    if username in password_manager.keys() and password_manager[username] == hashed_password:
+        print('Login successful!')
+    else:
+        print('Invalid username or password') 
+
+def main():
+    while True:
+        choice = input('Enter 1 to create an account, 2 to login, or 0 to exit: ')
+        if choice == '1':
+            create_account()
+        elif choice == '2':
+            login()
+        elif choice == '0':
+            print('Have a nice day!')
+            break
+        else:
+            print('Invalid choice!')
+
+if __name__ == "__main__":
+    main()
